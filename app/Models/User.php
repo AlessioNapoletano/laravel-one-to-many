4dfaff0873dd45178a45637ba1930db21320c387
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-03-01 00:14:55 +0100

    Add relationship method 'userProfile' into class 'User' model

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * relationship method
+     * 
+     */
+    public function userProfile() {
+        return $this->hasOne(User_profile::class);
+    }
 }
